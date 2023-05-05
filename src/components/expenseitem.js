84318eb13698e9162e1c8c7eca0b84686ec61d65
vishBhat14 <vishweshbhat123@gmail.com>
2023-05-05 19:02:53 +0530

    firstcommit

---
+++ src/components/expenseitem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2> Expense items!</h2>
+      <h3>Food Rs 10</h3>
+
+      <h3>Petrol Rs 100</h3>
+
+      <h3>Movies Rs 200</h3>
+    </div>
+  );
+}
+export default ExpenseItem;
